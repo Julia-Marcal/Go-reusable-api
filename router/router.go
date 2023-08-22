951b62d4202c2773c9e725145d83323a0c1c0fae
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-08-22 12:05:21 -0300

    separating router from main

---
+++ router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	controllers "github.com/Julia-Marcal/reusable-api/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func Start_Router() {
+	router := gin.Default()
+	router.GET("users/:id", controllers.GetUser)
+	router.DELETE("users/:id", controllers.DeleteUser)
+	router.GET("users/", controllers.GetAllUsers)
+	router.POST("users/", controllers.CreateUser)
+	router.Run()
+}
