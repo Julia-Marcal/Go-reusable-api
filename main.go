d3698c2428735a090babf826faea0b9873e48266
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-08-23 18:21:02 -0300

    start db function add

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	router "github.com/Julia-Marcal/reusable-api/router"
+	newdb "github.com/Julia-Marcal/reusable-api/services"
 )
 
 func main() {
+	newdb.NewDB()
 	router.Start_Router()
 }
