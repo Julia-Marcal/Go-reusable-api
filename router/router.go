b89954541be175a750e90e6584faf71b4a6fcb06
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-08-23 18:23:48 -0300

    new router endpoint for login

--- router/router.go
+++ router/router.go
@@ -8,6 +8,7 @@ import (
 func Start_Router() {
 	router := gin.Default()
 	router.GET("users/:id", controllers.GetUser)
+	router.GET("login/:id", controllers.GetPass)
 	router.DELETE("users/:id", controllers.DeleteUser)
 	router.GET("users/", controllers.GetAllUsers)
 	router.POST("users/", controllers.CreateUser)
