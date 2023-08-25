fe0c95cf02ee186d6d417650ae761fd5f12db257
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-08-25 11:35:06 -0300

    developing branch 1st commit

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	router "github.com/Julia-Marcal/reusable-api/router"
-	newdb "github.com/Julia-Marcal/reusable-api/services"
 )
 
 func main() {
-	newdb.NewDB()
 	router.Start_Router()
 }
--- services/security/hashing.go
+++ services/security/hashing.go
@@ -2,39 +2,38 @@ package security
 
 import (
 	"crypto/rand"
-	"golang.org/x/crypto/scrypt"
+
 	env "github.com/Julia-Marcal/reusable-api/services/env"
+	"golang.org/x/crypto/scrypt"
 )
 
 func DeriveKey(password string, salt []byte) ([]byte, []byte, error) {
-    if salt == nil {
-        salt = make([]byte, 32)
-        if _, err := rand.Read(salt); err != nil {
-            return nil, nil, err
-        }
-    }
+	if salt == nil {
+		salt = make([]byte, 32)
+		if _, err := rand.Read(salt); err != nil {
+			return nil, nil, err
+		}
+	}
 
-    password_byte := []byte(password)
-    key, err := scrypt.Key(password_byte, salt, 1048576, 8, 1, 32)
-    if err != nil {
-        return nil, nil, err
-    }
+	password_byte := []byte(password)
+	key, err := scrypt.Key(password_byte, salt, 1048576, 8, 1, 32)
+	if err != nil {
+		return nil, nil, err
+	}
 
-    return key, salt, nil
+	return key, salt, nil
 }
-
-
 
 func LoginSystem(password string, password_db string) (bool, error) {
 	salt := env.SetSalt()
 
-	password_compare, _,err := DeriveKey(password, salt)
+	password_compare, _, err := DeriveKey(password, salt)
 	if err != nil {
 		return false, err
 	}
 
 	password_compare_str := string(password_compare)
-	if (password_compare_str == password_db) {
+	if password_compare_str == password_db {
 		return true, nil
 	}
 
