4150b37a7a1a5326336d17336bea47dc045360ad
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-08-26 17:33:39 -0300

    connecting to postgres

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	db "github.com/Julia-Marcal/reusable-api/repository"
 	router "github.com/Julia-Marcal/reusable-api/router"
 )
 
 func main() {
+	db.NewPostgres()
 	router.Start_Router()
 }
