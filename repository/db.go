e967bb9734e539fde6857263949fada19d4fae96
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-08-26 17:36:58 -0300

    using postgres as database

--- repository/db.go
+++ repository/db.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 
 	database "github.com/Julia-Marcal/reusable-api/database"
-	"gorm.io/driver/sqlite"
+	env "github.com/Julia-Marcal/reusable-api/services/env"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func NewSqlite() *gorm.DB {
+func NewPostgres() *gorm.DB {
+	conStr := env.SetPgConnection()
 	fmt.Println("about to connect to database")
-	db, err := gorm.Open(sqlite.Open("golang-db.sqlite"), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(conStr), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect to database")
 	}
